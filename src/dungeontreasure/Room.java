68e76b847e6159d337f020fa935abccbe30f22da
ÄGARE <GARE@DESKTOP-9FCGT42>
2022-12-01 12:06:36 +0100

    add doNarrative function

--- src/dungeontreasure/Room.java
+++ src/dungeontreasure/Room.java
@@ -9,5 +9,16 @@ package dungeontreasure;
  * @author Ronja
  */
 public class Room {
+    private String roomDesc;
+    private Door[] doors;
     
+    public Room(String roomDesc, Door[] doors) {
+        this.roomDesc = roomDesc;
+        this.doors = doors;
+    }
+    
+    public void doNarrative() {
+        System.out.println(roomDesc);
+        
+    }
 }
