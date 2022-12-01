a9c48a5c9c984bab5bdaebebf5e214746847507b
ÄGARE <GARE@DESKTOP-9FCGT42>
2022-12-01 15:06:46 +0100

    Create all rooms and setup game

--- src/dungeontreasure/DungeonTreasure.java
+++ src/dungeontreasure/DungeonTreasure.java
@@ -19,30 +19,50 @@ public class DungeonTreasure {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
+        ArrayList<Room> rooms = new ArrayList<>();
         
         //Getting the username
         System.out.println("Welcome to Dragon Treasure, enter your username and press enter to start a new game");
         Player playerName = new Player(input.nextLine());
         //playGame(playerName)
-        setupGame();
+        setupGame(rooms);
+        Dungeon dungeon = new Dungeon(rooms, "Oh no! The entrance collapsed behind you " + playerName.getName());
+        dungeon.playGame();
+        System.out.println("Pjuh, looks like you made it out alive!");
 
     }
-    public static void setupGame() {
+    public static void setupGame(ArrayList<Room> rooms) {
         // Creating all doors
         Door sö = new Door('s' , false);
         Door norr = new Door('n' , false);
         Door öst = new Door('ö' , false);
         Door väst = new Door('v' , false);
+        Door utgång = new Door('u', false);
         
         Door[] sv = {sö, väst};
+        Door[] nv = {norr, väst};
         Door[] sn = {sö, norr};
         Door[] ös = {sö, öst};
-         
-        ArrayList<Room> rooms = new ArrayList<>();
-        Room room1 = new Room("Du går in i grottan och dörren kollapsar bakom dig", sn);
-        rooms.add(room1);
-        Room room2 = new Room("Du är i ett annat rum", ös);
-        rooms.add(room2);
-        room1.doNarrative();
+        Door[] vö = {väst, öst};
+        Door[] nö = {norr, öst};
+        Door[] nsö = {sö, öst, norr};
+        Door[] söv = {sö, väst, öst};
+        Door[] ut = {utgång, väst, norr};
+        
+        
+        //Creating all rooms
+        rooms.add(new Room("Du är i rum 1", sn));
+        rooms.add(new Room("Du är i rum 2", ös));
+        rooms.add(new Room("Du är i rum 3", vö));
+        rooms.add(new Room("Du är i rum 4", söv));
+        rooms.add(new Room("Du är i rum 5", sv));
+        rooms.add(new Room("Du är i rum 6", nv));
+        rooms.add(new Room("Du är i rum 7", nsö));
+        rooms.add(new Room("Du är i rum 8", ut));
+        rooms.add(new Room("Du är i rum 9", vö));
+        rooms.add(new Room("Du är i rum 10", nö));
+        
+
+
     }
 }
