18c2623b916e1397198e0ea8e3a8986735740732
ÄGARE <GARE@DESKTOP-9FCGT42>
2022-12-01 15:06:13 +0100

    First draft, printing out every room

--- src/dungeontreasure/Dungeon.java
+++ src/dungeontreasure/Dungeon.java
@@ -4,18 +4,33 @@
  */
 package dungeontreasure;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Ronja
  */
 public class Dungeon {
-    private int currentRoom;
+    private ArrayList<Room> rooms;
+    private Room currentRoom;
     private String welcomeMsg;
     
-    public Dungeon(int currentRoom, int Room) {
-        this.currentRoom = Room;
+    public Dungeon(ArrayList<Room> rooms, String welcomeMsg) {
+        this.rooms = rooms;
+        this.welcomeMsg = welcomeMsg;
     }
+
     public void playGame() {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(welcomeMsg);
+        // Setting currentRoom to first room in cave
+        currentRoom = rooms.get(0);
+        for (Room room : rooms) {
+            currentRoom = room;
+            currentRoom.doNarrative();
+            String choise = input.nextLine();
+        }
     }
 }
